cac0b1d6ad8926956a74941a6bb79bc00d772fab
Thiago Rocha <thiagord2016@gmail.com>
2022-09-21 02:58:54 +0000

    Update index.tsx and pag2.tsx

--- pages/pag2.tsx
+++ pages/pag2.tsx
@@ -1,6 +1,19 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const odsObjects = [
+  {
+    number: "ODS #1",
+    name: "Erradicação da Pobreza",
+    color: "red-500",
+  },
+  {
+    number: "ODS #2",
+    name: "Fome zero e agricultura sustentável",
+    color: "yellow-300",
+  },
+];
 
 const Home: NextPage = () => {
   return (
@@ -12,58 +25,48 @@ const Home: NextPage = () => {
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Página 2{' '}
+          Página 2{" "}
           <a className="text-blue-600" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
             pages/index.tsx
           </code>
         </p>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+          {odsObjects.map((item) => (
+            <a
+              href="#"
+              className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            >
+              {/* exite um item.color que é o background
+                 que eu quero que seja em cada item */}
+              <h3 className="text-2xl font-bold">{item.number}</h3>
+              <p className="mt-4 text-xl">{item.name}</p>
+            </a>
+          ))}
+
+          <p className="mt-12 text-2xl"> Como eu quero que fique: </p>
+
           <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="#"
+            className="mt-6 w-96 rounded-xl border p-6 text-left bg-red-500 hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
+            <h3 className="text-2xl font-bold">ODS #1</h3>
+            <p className="mt-4 text-xl">Erradicação da Pobreza</p>
           </a>
 
           <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="#"
+            className="mt-6 w-96 rounded-xl border p-6 text-left bg-yellow-300 hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3 className="text-2xl font-bold">ODS #2</h3>
+            <p className="mt-4 text-xl">Fome zero e agricultura sustentável</p>
           </a>
         </div>
       </main>
@@ -75,12 +78,12 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
