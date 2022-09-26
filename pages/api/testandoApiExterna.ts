0b3826e45ff99478a0c08f739b0daf2343cb52a3
Thiago Rocha <thiagord2016@gmail.com>
2022-09-26 13:53:23 -0300

    fix: deploy error

--- pages/api/testandoApiExterna.ts
+++ pages/api/testandoApiExterna.ts
@@ -3,20 +3,19 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   date: string,
-  name: string
+  nome: string
 }
 
 async function testando(req: NextApiRequest, res: NextApiResponse<Data>) {
   const dynamicDate = new Date();
 
-  const testeAPI = await fetch("https://www.fruityvice.com/api/fruit/all");
+  const testeAPI = await fetch("https://api.github.com/users/thiagorochatr");
   const testeAPIJson = await testeAPI.json();
-  const arrayMenor = testeAPIJson.slice(0,6);
-  const nomes = arrayMenor.map(item => { return item.name });
+  const nomeNaApi = testeAPIJson.name;
 
   res.json({
     date: dynamicDate.toUTCString(),
-    name: nomes
+    nome: nomeNaApi
   });
 }
 
